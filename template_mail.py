987f391670594b99eb2788f0448a5a7a3e02189e
unknown <finite.impulse@gmail.com>
2021-07-21 16:34:36 +0200

    Flask Mail Template

--- template_mail.py
+++ template_mail.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 # https://pythonhosted.org/Flask-Mail/
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'spof.flood@gmail.com'
-app.config['MAIL_PASSWORD'] = 'c3Nrt8VdE3ToYNIBEmVl'
+app.config['MAIL_USERNAME'] = 'your_email@mail.com'
+app.config['MAIL_PASSWORD'] = 'your_password'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
@@ -19,7 +19,7 @@ mail = Mail(app)
 # message object mapped to the index route
 @app.route("/")
 def index():
-   msg = Message('Hello, this is a test', sender ='spof.flood@gmail.com', recipients = ['xixeko6152@dedatre.com'])
+   msg = Message('Hello, this is a test', sender ='your_email@mail.com', recipients = ['target_email@mail.com'])
    msg.body = 'This is a Flask message sent from Flask-Mail'
    mail.send(msg)
 
